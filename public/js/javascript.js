dca09a3a987602d3660b8a40237b741e2dc5aa87
xdmat <xdmateo90@gmail.com>
2022-11-10 15:58:19 -0500

    svg rotate en js

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -8,6 +8,11 @@ const idTexto4 = document.querySelector("#texto4");
 const pregunta4 = document.querySelector("#pregunta4");
 const idTexto5 = document.querySelector("#texto5");
 const pregunta5 = document.querySelector("#pregunta5");
+const svg1 = document.querySelector("#svg1");
+const svg2 = document.querySelector("#svg2");
+const svg3 = document.querySelector("#svg3");
+const svg4 = document.querySelector("#svg4");
+const svg5 = document.querySelector("#svg5");
 
 pregunta1.addEventListener("click", abriridTexto1);
 pregunta2.addEventListener("click", abriridTexto2);
@@ -21,11 +26,13 @@ function abriridTexto1() {
   if (idTextoClosed) {
     idTexto1.classList.remove("hidden");
     pregunta1.classList.add("font-bold");
+    svg1.classList.add("-scale-y-100");
   }
 
   if (!idTextoClosed) {
     idTexto1.classList.add("hidden");
     pregunta1.classList.remove("font-bold");
+    svg1.classList.remove("-scale-y-100");
   }
 }
 
@@ -35,11 +42,13 @@ function abriridTexto2() {
   if (idTextoClosed) {
     idTexto2.classList.remove("hidden");
     pregunta2.classList.add("font-bold");
+    svg2.classList.add("-scale-y-100");
   }
 
   if (!idTextoClosed) {
     idTexto2.classList.add("hidden");
     pregunta2.classList.remove("font-bold");
+    svg2.classList.remove("-scale-y-100");
   }
 }
 
@@ -49,11 +58,13 @@ function abriridTexto3() {
   if (idTextoClosed) {
     idTexto3.classList.remove("hidden");
     pregunta3.classList.add("font-bold");
+    svg3.classList.add("-scale-y-100");
   }
 
   if (!idTextoClosed) {
     idTexto3.classList.add("hidden");
     pregunta3.classList.remove("font-bold");
+    svg3.classList.remove("-scale-y-100");
   }
 }
 
@@ -63,11 +74,13 @@ function abriridTexto4() {
   if (idTextoClosed) {
     idTexto4.classList.remove("hidden");
     pregunta4.classList.add("font-bold");
+    svg4.classList.add("-scale-y-100");
   }
 
   if (!idTextoClosed) {
     idTexto4.classList.add("hidden");
     pregunta4.classList.remove("font-bold");
+    svg4.classList.remove("-scale-y-100");
   }
 }
 
@@ -77,11 +90,13 @@ function abriridTexto5() {
   if (idTextoClosed) {
     idTexto5.classList.remove("hidden");
     pregunta5.classList.add("font-bold");
+    svg5.classList.add("-scale-y-100");
   }
 
   if (!idTextoClosed) {
     idTexto5.classList.add("hidden");
     pregunta5.classList.remove("font-bold");
+    svg5.classList.remove("-scale-y-100");
   }
   console.log(idTexto5);
 }
